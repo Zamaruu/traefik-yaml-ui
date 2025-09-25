013b81b5d04397653abc7f93cb69ef2011d24f27
Maximilian Ditz <max.ditz@outlook.de>
2025-09-25 10:18:42 +0200

    re-add simpler typestructure for poc in frontend and first api getter endpoint for sample yaml

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,32 @@
+import fs from "fs";
+import yaml from "js-yaml";
+import dotenv from "dotenv";
+import { TraefikConfig } from "./models/types";
 import express, { Request, Response } from "express";
 
+dotenv.config();
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+const configPath = process.env.TRAEFIK_CONFIG_PATH;
+
+if (!configPath) {
+    throw new Error("TRAEFIK_CONFIG_PATH not set in .env file!");
+}
 
 // Beispiel-Endpunkt mit Typen
-app.get("/api/hello", (req: Request, res: Response): void => {
-    res.json({ message: "Hallo von der Express API mit TypeScript 🚀" });
+app.get("/config", (req: Request, res: Response) => {
+    try {
+        const fileContent = fs.readFileSync(configPath, "utf8");
+        const parsed = yaml.load(fileContent) as TraefikConfig;
+
+        res.json(parsed);
+    } catch (err) {
+        console.error("Fehler beim Laden der Config:", err);
+        res.status(500).json({ error: "Konnte Config nicht laden" });
+    }
 });
 
-app.listen(PORT, () => {
-    console.log(`✅ Server läuft auf http://localhost:${PORT}`);
+app.listen(port, () => {
+    console.log(`✅ Server läuft auf http://localhost:${port}`);
 });
--- frontend/lib/src/models/types.dart
+++ frontend/lib/src/models/types.dart
@@ -1,132 +1,291 @@
-// traefik_config.dart
+class DynamicConfig {
+  Http? http;
 
-class TraefikConfig {
-  final HttpConfig http;
+  DynamicConfig({this.http});
 
-  TraefikConfig({required this.http});
-
-  factory TraefikConfig.fromJson(Map<String, dynamic> json) {
-    return TraefikConfig(
-      http: HttpConfig.fromJson(json['http']),
-    );
+  DynamicConfig.fromJson(Map<String, dynamic> json) {
+    http = json['http'] != null ? Http.fromJson(json['http']) : null;
   }
-}
 
-class HttpConfig {
-  final Map<String, Middleware>? middlewares;
-  final Map<String, ServersTransport>? serversTransports;
-  final Map<String, Service>? services;
-  final Map<String, Router>? routers;
-
-  HttpConfig({
-    this.middlewares,
-    this.serversTransports,
-    this.services,
-    this.routers,
-  });
-
-  factory HttpConfig.fromJson(Map<String, dynamic> json) {
-    return HttpConfig(
-      middlewares: (json['middlewares'] as Map?)?.map(
-            (k, v) => MapEntry(k as String, Middleware.fromJson(v)),
-      ),
-      serversTransports: (json['serversTransports'] as Map?)?.map(
-            (k, v) => MapEntry(k as String, ServersTransport.fromJson(v)),
-      ),
-      services: (json['services'] as Map?)?.map(
-            (k, v) => MapEntry(k as String, Service.fromJson(v)),
-      ),
-      routers: (json['routers'] as Map?)?.map(
-            (k, v) => MapEntry(k as String, Router.fromJson(v)),
-      ),
-    );
-  }
-}
-
-class Middleware {
-  final List<String>? sourceRange;
-  final String? address;
-  final bool? trustForwardHeader;
-
-  Middleware({this.sourceRange, this.address, this.trustForwardHeader});
-
-  factory Middleware.fromJson(Map<String, dynamic> json) {
-    if (json.containsKey('ipAllowList')) {
-      return Middleware(
-        sourceRange:
-        (json['ipAllowList']['sourceRange'] as List).cast<String>(),
-      );
-    } else if (json.containsKey('forwardauth')) {
-      return Middleware(
-        address: json['forwardauth']['address'],
-        trustForwardHeader: json['forwardauth']['trustForwardHeader'],
-      );
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (http != null) {
+      data['http'] = http!.toJson();
     }
-    return Middleware();
+    return data;
   }
 }
 
-class ServersTransport {
-  final bool? insecureSkipVerify;
+class Http {
+  List<Service>? services;
+  List<Router>? routers;
+  Middlewares? middlewares;
+  ServersTransports? serversTransports;
 
-  ServersTransport({this.insecureSkipVerify});
+  Http({this.middlewares, this.serversTransports, this.services, this.routers});
 
-  factory ServersTransport.fromJson(Map<String, dynamic> json) {
-    return ServersTransport(
-      insecureSkipVerify: json['insecureSkipVerify'],
-    );
+  Http.fromJson(Map<String, dynamic> json) {
+    middlewares = json['middlewares'] != null ? Middlewares.fromJson(json['middlewares']) : null;
+    serversTransports = json['serversTransports'] != null ? ServersTransports.fromJson(json['serversTransports']) : null;
+
+    if (json['services'] != null) {
+      services = <Service>[];
+      json['services'].forEach((v) {
+        services!.add(Service.fromJson(v));
+      });
+    }
+
+    if (json['routers'] != null) {
+      routers = <Router>[];
+      json['routers'].forEach((v) {
+        routers!.add(Router.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (middlewares != null) {
+      data['middlewares'] = middlewares!.toJson();
+    }
+    if (serversTransports != null) {
+      data['serversTransports'] = serversTransports!.toJson();
+    }
+    if (services != null) {
+      data['servers'] = services!.map((v) => v.toJson()).toList();
+    }
+    if (routers != null) {
+      data['routers'] = routers!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Middlewares {
+  Ipallow? ipAllow;
+  CrowdsecBouncer? crowdsecBouncer;
+
+  Middlewares({this.ipAllow, this.crowdsecBouncer});
+
+  Middlewares.fromJson(Map<String, dynamic> json) {
+    ipAllow = json['ipallow'] != null ? Ipallow.fromJson(json['ipallow']) : null;
+    crowdsecBouncer = json['crowdsec-bouncer'] != null ? CrowdsecBouncer.fromJson(json['crowdsec-bouncer']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (ipAllow != null) {
+      data['ipallow'] = ipAllow!.toJson();
+    }
+    if (crowdsecBouncer != null) {
+      data['crowdsec-bouncer'] = crowdsecBouncer!.toJson();
+    }
+    return data;
+  }
+}
+
+class Ipallow {
+  IpAllowList? ipAllowList;
+
+  Ipallow({this.ipAllowList});
+
+  Ipallow.fromJson(Map<String, dynamic> json) {
+    ipAllowList = json['ipAllowList'] != null ? IpAllowList.fromJson(json['ipAllowList']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (ipAllowList != null) {
+      data['ipAllowList'] = ipAllowList!.toJson();
+    }
+    return data;
+  }
+}
+
+class IpAllowList {
+  List<String>? sourceRange;
+
+  IpAllowList({this.sourceRange});
+
+  IpAllowList.fromJson(Map<String, dynamic> json) {
+    sourceRange = json['sourceRange'].cast<String>();
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['sourceRange'] = sourceRange;
+    return data;
+  }
+}
+
+class CrowdsecBouncer {
+  Forwardauth? forwardauth;
+
+  CrowdsecBouncer({this.forwardauth});
+
+  CrowdsecBouncer.fromJson(Map<String, dynamic> json) {
+    forwardauth = json['forwardauth'] != null ? Forwardauth.fromJson(json['forwardauth']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (forwardauth != null) {
+      data['forwardauth'] = forwardauth!.toJson();
+    }
+    return data;
+  }
+}
+
+class Forwardauth {
+  String? address;
+  bool? trustForwardHeader;
+
+  Forwardauth({this.address, this.trustForwardHeader});
+
+  Forwardauth.fromJson(Map<String, dynamic> json) {
+    address = json['address'];
+    trustForwardHeader = json['trustForwardHeader'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['address'] = address;
+    data['trustForwardHeader'] = trustForwardHeader;
+    return data;
+  }
+}
+
+class ServersTransports {
+  InsecureTransport? insecureTransport;
+
+  ServersTransports({this.insecureTransport});
+
+  ServersTransports.fromJson(Map<String, dynamic> json) {
+    insecureTransport = json['insecureTransport'] != null ? InsecureTransport.fromJson(json['insecureTransport']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (insecureTransport != null) {
+      data['insecureTransport'] = insecureTransport!.toJson();
+    }
+    return data;
+  }
+}
+
+class InsecureTransport {
+  bool? insecureSkipVerify;
+
+  InsecureTransport({this.insecureSkipVerify});
+
+  InsecureTransport.fromJson(Map<String, dynamic> json) {
+    insecureSkipVerify = json['insecureSkipVerify'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['insecureSkipVerify'] = insecureSkipVerify;
+    return data;
   }
 }
 
 class Service {
-  final List<Server> servers;
-  final String? serversTransport;
+  LoadBalancer? loadBalancer;
 
-  Service({required this.servers, this.serversTransport});
+  Service({this.loadBalancer});
 
-  factory Service.fromJson(Map<String, dynamic> json) {
-    return Service(
-      servers: (json['loadBalancer']['servers'] as List)
-          .map((s) => Server.fromJson(s))
-          .toList(),
-      serversTransport: json['loadBalancer']['serversTransport'],
-    );
+  Service.fromJson(Map<String, dynamic> json) {
+    loadBalancer = json['loadBalancer'] != null ? LoadBalancer.fromJson(json['loadBalancer']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (loadBalancer != null) {
+      data['loadBalancer'] = loadBalancer!.toJson();
+    }
+    return data;
   }
 }
 
-class Server {
-  final String url;
+class LoadBalancer {
+  List<Servers>? servers;
 
-  Server({required this.url});
+  LoadBalancer({this.servers});
 
-  factory Server.fromJson(Map<String, dynamic> json) {
-    return Server(url: json['url']);
+  LoadBalancer.fromJson(Map<String, dynamic> json) {
+    if (json['servers'] != null) {
+      servers = <Servers>[];
+      json['servers'].forEach((v) {
+        servers!.add(Servers.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (servers != null) {
+      data['servers'] = servers!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Servers {
+  String? url;
+
+  Servers({this.url});
+
+  Servers.fromJson(Map<String, dynamic> json) {
+    url = json['url'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['url'] = url;
+    return data;
+  }
+}
+
+class Tls {
+  String? certresolver;
+
+  Tls({this.certresolver});
+
+  Tls.fromJson(Map<String, dynamic> json) {
+    certresolver = json['certresolver'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['certresolver'] = certresolver;
+    return data;
   }
 }
 
 class Router {
-  final String rule;
-  final String service;
-  final List<String> entryPoints;
-  final List<String>? middlewares;
-  final String? certresolver;
+  String? rule;
+  String? service;
+  List<String>? entryPoints;
+  List<String>? middlewares;
+  Tls? tls;
 
-  Router({
-    required this.rule,
-    required this.service,
-    required this.entryPoints,
-    this.middlewares,
-    this.certresolver,
-  });
+  Router({this.rule, this.service, this.entryPoints, this.tls});
 
-  factory Router.fromJson(Map<String, dynamic> json) {
-    return Router(
-      rule: json['rule'],
-      service: json['service'],
-      entryPoints: (json['entryPoints'] as List).cast<String>(),
-      middlewares:
-      (json['middlewares'] as List?)?.map((e) => e.toString()).toList(),
-      certresolver: json['tls']?['certresolver'],
-    );
+  Router.fromJson(Map<String, dynamic> json) {
+    rule = json['rule'];
+    service = json['service'];
+    entryPoints = json['entryPoints'].cast<String>();
+    tls = json['tls'] != null ? Tls.fromJson(json['tls']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['rule'] = rule;
+    data['service'] = service;
+    data['entryPoints'] = entryPoints;
+    if (tls != null) {
+      data['tls'] = tls!.toJson();
+    }
+    return data;
   }
 }
